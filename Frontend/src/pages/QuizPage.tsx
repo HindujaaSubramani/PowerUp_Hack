58aa4b476f2fad009844da4034a0cf66af02fe39
Hindujaa Subramani <hindujaas.cse2023@citchennai.net>
2025-07-26 21:45:01 +0530

    Updated API call

--- Frontend/src/pages/QuizPage.tsx
+++ Frontend/src/pages/QuizPage.tsx
@@ -30,7 +30,8 @@ const QuizPage = () => {
       }
 
       try {
-        const response = await fetch("http://localhost:5000/generate_quiz", {
+        const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+        const response = await fetch(`${API_URL}/generate_quiz`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
